8e1930163924c7f306214d6491eb5f03910da8b0
Basant (Kitto) <basantmandal@gmail.com>
2022-06-16 11:04:28 +0530

    Update Namespace

--- registration.php
+++ registration.php
@@ -13,8 +13,6 @@
  * @license         For License please read license.txt
  */
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'HK2_ScrollTop',
-    __DIR__
-);
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'HK2_ScrollTop', __DIR__);
